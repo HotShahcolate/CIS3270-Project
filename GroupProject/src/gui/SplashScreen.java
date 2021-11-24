4e08447c64a97ad7869531be9c7552ab6bbaf141
nhatt <nhatt@DESKTOP-I4JH5LF>
2021-11-23 20:57:39 -0500

    Add switch scene

--- GroupProject/src/gui/SplashScreen.java
+++ GroupProject/src/gui/SplashScreen.java
@@ -2,15 +2,17 @@ package gui;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
-import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class SplashScreen extends Application implements EventHandler <ActionEvent> {
+public class SplashScreen extends Application {
+	Scene splash, login;
 	public static void main (String[] args) {
 		launch(args);
 	}
@@ -23,7 +25,7 @@ public class SplashScreen extends Application implements EventHandler <ActionEve
 		Pane pane = new Pane();
 		
 		Image logo = new Image ("https://i.imgur.com/wy1aFGR.png");
-		Scene scene = new Scene(pane, 500, 500);
+		splash = new Scene(pane, 500, 500);
 		ImageView view = new ImageView(logo);
 		
 		pane.getChildren().add(view);
@@ -31,30 +33,29 @@ public class SplashScreen extends Application implements EventHandler <ActionEve
 		
 		mainStage.setTitle("Welcome to Vista Air");
 		
-		mainStage.setScene(scene);
+		
 		
 
 		button = new Button();
 		button.setText("Start");
 		button.setLayoutX(225);
 		button.setLayoutY(350);
-		
-		
-		button.setOnAction(this);
-		
-		
 		pane.getChildren().add(button);
+		
+		
+		button.setOnAction(e -> mainStage.setScene(login));
+		
+		StackPane pane2 = new StackPane();
+		login = new Scene(pane2, 500, 500);
+		Label label2 = new Label("Log In To Continue Your Journey");
+		pane2.getChildren().add(label2);
+		
+	
+		mainStage.setScene(splash);
 		mainStage.show();
 		
 	}
-	public void handle (ActionEvent event) {
-		if(event.getSource()== button) {
-			
-		}
-	}
-	
-	
-	
+
 }
 
 
