1cd55578bfd23494dac231ae557e5e86a482b86a
nhatt <nhatt@10.250.18.77>
2021-12-01 16:40:40 -0500

    Scene Builder Update

--- GroupProject/src/SceneBuilder/application.java
+++ GroupProject/src/SceneBuilder/application.java
@@ -13,18 +13,16 @@ import javafx.scene.layout.Pane;
 import javafx.beans.binding.Bindings; 
 
 
-
 public class application extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-	
+		
 			FXMLLoader load_Login = new FXMLLoader(getClass().getResource("Login_Page.fxml"));
 			Pane root_Login = load_Login.load();
 			Scene scene_Login = new Scene(root_Login,600,400);
 			root_Login.prefWidthProperty().bind(root_Login.widthProperty());
 			root_Login.prefHeightProperty().bind(root_Login.heightProperty());
-			
-			
+					
 			FXMLLoader Splash_Load = new FXMLLoader(getClass().getResource("SplashScreen.fxml"));
 			Pane root_Splash = Splash_Load.load();
 			Scene scene_Splash = new Scene(root_Splash,600,400);
@@ -38,6 +36,7 @@ public class application extends Application {
 			root_Signup.prefWidthProperty().bind(root_Signup.widthProperty());
 			root_Signup.prefHeightProperty().bind(root_Signup.heightProperty());
 			
+			
 			FXMLLoader load_Signup_Cont = new FXMLLoader(getClass().getResource("SignUp Cont.fxml"));
 			Pane root_Signup_Cont = load_Signup_Cont.load();
 			Scene scene_Signup_Cont = new Scene(root_Signup_Cont,600,400);
@@ -50,16 +49,25 @@ public class application extends Application {
 			root_Forget.prefWidthProperty().bind(root_Forget.widthProperty());
 			root_Forget.prefHeightProperty().bind(root_Forget.heightProperty());
 			
+			FXMLLoader load_Main = new FXMLLoader(getClass().getResource("Main_Page.fxml"));
+			Pane root_Main = load_Main.load();
+			Scene scene_Main = new Scene(root_Main,600,400);
+			root_Main.prefWidthProperty().bind(root_Main.widthProperty());
+			root_Main.prefHeightProperty().bind(root_Main.heightProperty());
+			
+			
+					
+			controller loginLoader = (controller)Splash_Load.getController();
+			loginLoader.setLogin(scene_Login);
+			loginLoader.setImage(root_Splash);
+			
 			controller2 signupLoader = (controller2)load_Login.getController();
 			signupLoader.setSignUpScene(scene_Signup);
 			signupLoader.setImage(root_Login);
 			signupLoader.setStack(root_Login);
 			signupLoader.setForgetPassword(scene_Forget);
+			signupLoader.setMain(scene_Main);
 			
-			
-			controller loginLoader = (controller)Splash_Load.getController();
-			loginLoader.setLogin(scene_Login);
-			loginLoader.setImage(root_Splash);
 			
 			controller3 signupContLoader = (controller3)load_Signup.getController();
 			signupContLoader.setSignUp(scene_Signup_Cont);
@@ -69,20 +77,23 @@ public class application extends Application {
 			controller4 signupCont = (controller4)load_Signup_Cont.getController();
 			signupCont.setBox(root_Signup_Cont);
 			signupCont.setImage(root_Signup_Cont);
-			signupCont.choiceBox1();
-			signupCont.choiceBox2();
+			signupCont.comboBox1();
+			signupCont.comboBox2();
 			
 			controller5 forgetPassword = (controller5)load_Forget.getController();
 			forgetPassword.setBox(root_Forget);
 			forgetPassword.setImage(root_Forget);
-			forgetPassword.choiceBox1();
-			forgetPassword.choiceBox2();
+			forgetPassword.comboBox1();
+			forgetPassword.comboBox2();
 			forgetPassword.setLogin(scene_Login);
 			
+			controller6 mainLoader = (controller6)load_Main.getController();
+			mainLoader.setImage(root_Main);
+			mainLoader.setBox(root_Main);
 			
 			primaryStage.setScene(scene_Splash);
 			primaryStage.show();
-			
+		
 		
 	}
 	
--- GroupProject/src/SceneBuilder/controller4.java
+++ GroupProject/src/SceneBuilder/controller4.java
@@ -1,5 +1,6 @@
 package SceneBuilder;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -12,17 +13,17 @@ import javafx.fxml.Initializable;
 public class controller4 implements Initializable{
 	 @FXML private ImageView image;
 	 @FXML private StackPane stack_pane;
-	 @FXML private ChoiceBox<String> choice1;
-	 @FXML private ChoiceBox<String> choice2;
+	 @FXML private ComboBox<String> choice1;
+	 @FXML private ComboBox<String> choice2;
 	 
 	 
-	 public void choiceBox1 () {
+	 public void comboBox1 () {
 		 choice1.getItems().addAll ("What is your mom's maiden name?", "What is your first pet's name?", 
 				 "What is the name of your high school?");
 		 choice1.setValue("What is your mom's maiden name?");
 	 }
 	 
-	 public void choiceBox2 () {
+	 public void comboBox2 () {
 		 choice2.getItems().addAll ("What was the first company that you worked for?", 
 				 "What is the name of your favorite restaurant", 
 				 "What is the name of your middle school");
